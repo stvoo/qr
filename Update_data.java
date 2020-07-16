2d4942aecfbecc614fa51f6f9dda40b28c58ec8d
stvoo <68398853+stvoo@users.noreply.github.com>
2020-07-17 01:09:41 +0300

    Add files via upload

--- Update_data.java
+++ Update_data.java
@@ -1,4 +1,4 @@
-package qr_govyaha;
+package hui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -38,7 +38,7 @@ public class Update_data {
         String user = "postgres";
         String password = "12345";
         //String column1 = "status";
-        String new_data, new_dataa;
+        String new_data, new_dataa, new_dataaa;
         int id;
         int idd;
         //String error = "status";
@@ -69,10 +69,18 @@ public class Update_data {
             //Scanner in = new Scanner(System.in);
             //пегнкчьм
             System.out.print("Input new data for the field resolution: ");
-            //if (in.hasNext()) {
-            new_data = in.nextLine();
-            System.out.print("Input the id of the citizen: ");
-            id = in.nextInt();
+            if (in.hasNext()) {
+            	new_data = in.nextLine();
+            	System.out.print("Input the id of the citizen: ");
+            	try {
+                	while(!in.hasNextInt()) {
+                		System.out.println("That is not a valid input, try again.");
+                        in.nextInt();
+                	}
+                } catch (InputMismatchException e) {
+                	System.out.print(e.getMessage()); 
+                }
+                id = in.nextInt(); 
             String sql = "update users set resolution='" + new_data + " ' where id='" + id + "'";
             //in.close();
             //Scanner inn = new Scanner(System.in);
@@ -91,22 +99,38 @@ public class Update_data {
                 	System.out.print(e.getMessage()); 
                 }
                 id = in.nextInt(); 
+             String sql1 = "update users set status='" + new_dataa + " ' where id='" + id + "'";
+               
+             
+             System.out.print("Input new data for the field notes: ");
+                new_dataaa = in.nextLine();
+                if (in.hasNext()) {
+                    new_dataaa = in.nextLine();
+                    System.out.print("Input the id of the citizen: ");
+                    try {
+                    	while(!in.hasNextInt()) {
+                    		System.out.println("That is not a valid input, try again.");
+                            in.nextInt();
+                    	}
+                    } catch (InputMismatchException e) {
+                    	System.out.print(e.getMessage()); 
+                    }
+                    id = in.nextInt(); 
             
             //System.out.print("Input the id of the citizen: ");
             
             //idd = in.nextInt();
-            String sql1 = "update users set status='" + new_dataa + "' where id='" + id + "'";
+            String sql2 = "update users set notes='" + new_dataaa + " ' where id='" + id + "'";
             
             int rowsAffected = myStmt.executeUpdate(sql);
             int rowsAffected1 = myStmt.executeUpdate(sql1);
+            int rowsAffected2 = myStmt.executeUpdate(sql2);
             in.close();
  
             System.out.println("Rows affected: " + rowsAffected1);
             System.out.println("Update complete.");
             
-        }
-            //}
-        }
+        }}}}
       
     
         catch (Exception exc) {
